8305606d49e76fb0453e91cd03a8d16de6b46852
Ika rahma alia <ika418471@gmail.com>
2023-10-18 21:51:40 +0700

    modul 2

---
+++ app/Views/profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="d-flex justify-content-center align-items-center flex-column" style="height:100vh;width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/92287665?v=4" class="rounded-circle mx-auto d-block py-3" alt="..." width="160" height="190">
+
+    <table class="mx-auto">
+      <tr>
+        <td>
+          <button type="button" class="btn btn-secondary" style="width: 320px"><?= $nama ?></button>
+        </td>
+      </tr>
+      <tr>
+      <tr>
+        <td>
+          <button type="button" class="btn btn-secondary" style="width: 320px"><?= $npm?></button>
+        </td>
+      </tr>
+      <tr>
+        <td>
+        <button type="button" class="btn btn-secondary" style="width: 320px"><?= $kelas ?></button>
+        </td>
+      </tr>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
